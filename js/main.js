59dc844d65ac48f0e27596f04b23d391a6b676dd
Maichol <msibiriu@gmail.com>
2020-10-22 18:56:12 +0200

    problemi con il calcolo del vincitore

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@
 var user = prompt("inserisci una parola");
 console.log(user);
 // inversione parola
-// function invert(label) {
-//
-//   var nameReverse = label.split("").reverse().join("");
-//
-//   return nameReverse;
-// }
+function invert(label) {
+
+  var nameReverse = label.split("").reverse().join("");
+
+  return nameReverse;
+}
 // inversione parola con il loop
 function invert(label) {
   var nameReverse = "";
@@ -28,6 +28,8 @@ if (user == invertLabel) {
 else {
   console.log("la parola non e' palindroma");
 }
+
+
 // esercizio funzioni pari e dispari
 // scelta pari o dispari utente
 var palyer = prompt("scegli pari o dispari");
@@ -35,25 +37,35 @@ var palyer = prompt("scegli pari o dispari");
 var numberPlayer = parseInt( prompt("scegli un numero da 1 a 5") );
 // scelta numero computer
 function randomPc(min, max) {
-  var random = Math.floor( Math.random() * (max - min + 1) ) + min ;
+  var random = Math.floor( Math.random() * (5 + 1) ) + 1 ;
   return random;
 }
-
-
-
-
-
+// richiamo funzione computer
+var computer = randomPc(numberPlayer);
+console.log(computer);
+// somma numero utente con computer
 function sum(number) {
-   var result = numP + numC;
+   var result = numberPlayer + computer;
    return result;
 }
-
+// richiamo funzione somma
+var addition = sum(numberPlayer, computer);
+console.log(addition);
+// stabiliamo il valore somma
 function equalOdd(compare) {
-  if (sum % 2 == 0) {
+  if (compare % 2 == 0) {
     return equal;
   }
-
   return odd;
 }
+function value(winner) {
+   if ( (equal == palyer) || (odd == player) ) {
+     return winPlayer;
+   }
+   return winComputer;
+}
 // richiamo funzione
-var
+var win = value(winPlayer);
+console.log(win);
+var lose = value(winComputer);
+console.log(lose);
