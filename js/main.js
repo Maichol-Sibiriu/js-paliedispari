fd5fca0006c3bd5b96d22b7b07448e908a3c7425
Maichol <msibiriu@gmail.com>
2020-10-22 16:08:32 +0200

    inserito loop per inversione parola

--- js/main.js
+++ js/main.js
@@ -3,14 +3,25 @@
 var user = prompt("inserisci una parola");
 console.log(user);
 // inversione parola
-function invert(label) {
+// function invert(label) {
+//
+//   var nameReverse = "";
+//
+//   var nameReverse = label.split("").reverse().join("");
+//
+//   return nameReverse;
+// }
 
+// inversione parola con il loop
+function invert(label) {
   var nameReverse = "";
 
-  var nameReverse = label.split("").reverse().join("");
-
+  for (var i = label.length -1; i >= 0 ; i--) {
+     nameReverse += label[i];
+  }
   return nameReverse;
 }
+
 
 // richiamo funzione
 var invertLabel = invert(user);
